ab41f114d0265aeb8bd152964f6081cc5dfc2865
Matthew J. Sahagian <msahagian@dotink.org>
2015-10-20 03:50:06 -0700

    A fixed up base repository for better dependency injection

--- src/Repository.php
+++ src/Repository.php
@@ -1,5 +1,7 @@
 <?php namespace Inkwell\Doctrine
 {
+	use Dotink\Flourish;
+	use Doctrine\ORM\EntityManager;
 	use Doctrine\ORM\EntityRepository;
 
 	/**
@@ -8,12 +10,53 @@
 	 */
 	class Repository extends EntityRepository
 	{
+		const MODEL = NULL;
+
+
+		/**
+		 *
+		 */
+		static public $defaultOrder = array();
+
+
+		/**
+		 *
+		 */
+		public function __construct(EntityManager $entity_manager)
+		{
+			if (!static::MODEL) {
+				throw new Flourish\ProgrammerException('Must set model on repository class');
+			}
+
+			$this->model = static::MODEL;
+
+			parent::__construct($entity_manager, $entity_manager->getclassMetaData($this->model));
+		}
+
+
 		/**
 		 *
 		 */
 		public function create()
 		{
-			return new $this->getEntityName();
+			return new $this->model();
+		}
+
+
+		/**
+		 *
+		 */
+		public function save($entity, $flush = TRUE)
+		{
+			if (!($entity instanceof $this->model)) {
+				throw new Flourish\ProgrammerException();
+			}
+
+			$this->_em->persist($entity);
+
+			if ($flush) {
+				$this->_em->flush();
+			}
 		}
 	}
 }
