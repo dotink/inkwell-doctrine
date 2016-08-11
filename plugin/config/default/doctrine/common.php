6c6e160abd743f1954d6326c8bc22001dbc904c2
Matthew J. Sahagian <msahagian@dotink.org>
2016-08-10 23:36:48 -0700

    Fixing common config to include object manager + flush

--- plugin/config/default/doctrine/common.php
+++ plugin/config/default/doctrine/common.php
@@ -1,9 +1,21 @@
 <?php
 
-	return Affinity\Config::create(['quill'], [
+	return Affinity\Config::create(['quill', 'providers', 'middleware'], [
 		'@quill' => [
 			'commands' => [
 				'Inkwell\Doctrine\Command\OrmGenerateClassesCommand'
+			]
+		],
+
+		'@providers' => [
+			'mapping' => [
+				'Doctrine\Common\Persistence\ObjectManager' => 'Doctrine\ORM\EntityManager',
+			]
+		],
+
+		'@middleware' => [
+			'providers' => [
+				'Inkwell\Doctrine\Middleware\Flush'
 			]
 		]
 	]);
